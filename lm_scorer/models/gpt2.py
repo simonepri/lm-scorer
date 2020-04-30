ea4c0bc0bcb850e2b15a9e26f1c57c9190848a95
dldk-gael <gael@de-leseleuc.com>
2020-04-30 22:47:41 +0200

    revert to previous version - handle multiple sentences with for loop

--- lm_scorer/models/gpt2.py
+++ lm_scorer/models/gpt2.py
@@ -1,6 +1,5 @@
 from typing import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
-
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
@@ -19,89 +18,53 @@ class GPT2LMScorer(TransformersLMScorer):
         if "device" in options:
             self.model.to(options["device"])
 
-        self.batch_size = options["batch_size"] if "batch_size" in options else 1
-
-    def add_special_tokens_and_encode(self, text):
-        return self.tokenizer.encode(
-            self.tokenizer.bos_token + text + self.tokenizer.eos_token
-        )
-
-    def pad(self, sequences: List[torch.Tensor]):
-        max_seq_len = max([s.size(0) for s in sequences])
-        out_tensor = (
-            sequences[0]
-            .data.new_zeros(len(sequences), max_seq_len)
-            .fill_(self.tokenizer.eos_token_id)
-        )
-        mask = torch.zeros((len(sequences), max_seq_len), device=sequences[0].device)
-        for i, tensor in enumerate(sequences):
-            length = tensor.size(0)
-            out_tensor[i, :length] = tensor
-            mask[i, :length] = 1
-
-        return out_tensor, mask
-
-    def _tokens_log_prob_single_batch(
-        self, sentences: List[str]
-    ) -> List[Tuple[torch.FloatTensor, torch.LongTensor, List[str]]]:
-
+    # @overrides
+    def _tokens_log_prob_single_sentence(
+        self, text: str
+    ) -> Tuple[torch.FloatTensor, torch.LongTensor, List[str]]:
         device = self.model.device
-
-        tokens = [
-            self.add_special_tokens_and_encode(sentence) for sentence in sentences
-        ]
-        ids, mask = self.pad(
-            list(
-                map(
-                    lambda x: torch.tensor(  # pylint: disable=not-callable
-                        x, device=device, dtype=torch.long
-                    ),
-                    tokens,
-                )
-            )
+        input_text = "%s%s%s" % (
+            self.tokenizer.bos_token,
+            text,
+            self.tokenizer.eos_token,
+        )
+        # len(tokens) = seq_len + 2
+        tokens = self.tokenizer.tokenize(input_text)
+        # ids.shape = [1, seq_len + 2]
+        ids = torch.tensor(  # pylint: disable=not-callable
+            [self.tokenizer.convert_tokens_to_ids(tokens)],
+            device=device,
+            dtype=torch.long,
         )
 
         with torch.no_grad():
             outputs = self.model(ids)
 
-        # pred_scores.shape = [nb_sentences, max_seq_len, vocab_size]
+        # pred_scores.shape = [1, seq_len + 2, vocab_size]
         pred_scores = outputs[0]
 
-        # Align input and target
+        # len(tokens) = seq_len + 1
+        tokens = tokens[1:]
+        # ids.shape = [1, seq_len + 1, vocab_size]
         ids = ids[:, 1:]
+        # pred_scores.shape = [1, seq_len + 1, vocab_size]
         pred_scores = pred_scores[:, :-1, :]
 
-        # Retrieve the token scores corresponding to the target id
-        # ids_scores.shape = [nb_sentences, max_seq_len]
+        # ids_scores.shape = [1, seq_len + 1]
         ids_scores = pred_scores.gather(2, ids.unsqueeze(2)).squeeze(2)
-
-        # Zero the values of the padding inputs
-        ids_scores *= mask[:, 1:]
-
-        # log_prob.shape = [nb_sentences, max_seq_len]
+        # log_prob.shape = [1, seq_len + 1]
         log_probs = ids_scores - pred_scores.logsumexp(2)
 
-        return [
-            (log_probs[i, : len(tokens[i])], ids[i, : len(tokens[i])], tokens[i][1:])
-            for i in range(len(sentences))
-        ]  # type: ignore
+        return log_probs[0], ids[0], tokens  # type: ignore
 
     def _tokens_log_prob(
         self, sentences: List[str]
     ) -> List[Tuple[torch.FloatTensor, torch.LongTensor, List[str]]]:
+        return [
+            self._tokens_log_prob_single_sentence(sentence) for sentence in sentences
+        ]
 
-        output = []
-        for i in range(0, len(sentences), self.batch_size):
-            batch = sentences[i : i + self.batch_size]
-            output += self._tokens_log_prob_single_batch(batch)
-
-        if len(sentences) % self.batch_size != 0:
-            remaining_batch = sentences[-(len(sentences) % self.batch_size) :]
-            output += self._tokens_log_prob_single_batch(remaining_batch)
-
-        return output
-
-    # @overrides_tokens_log_prob_single_batch
+    # @overrides
     @classmethod
     def _supported_model_names(cls) -> Iterable[str]:
         return GPT2LMHeadModel.pretrained_model_archive_map.keys()
