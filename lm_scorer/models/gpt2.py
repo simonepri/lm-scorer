7f2ecd96bf9a0e16b4917017dba57bb0c2d0be63
dldk-gael <gael@de-leseleuc.com>
2020-05-01 15:06:34 +0200

    add input batching for gpt2

--- lm_scorer/models/gpt2.py
+++ lm_scorer/models/gpt2.py
@@ -1,7 +1,9 @@
 from typing import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
+
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from torch.nn.utils.rnn import pad_sequence
 
 from .abc.transformers import TransformersLMScorer
 
@@ -18,51 +20,78 @@ class GPT2LMScorer(TransformersLMScorer):
         if "device" in options:
             self.model.to(options["device"])
 
-    # @overrides
-    def _tokens_log_prob_single_sentence(
-        self, text: str
-    ) -> Tuple[torch.FloatTensor, torch.LongTensor, List[str]]:
+        self.batch_size = options["batch_size"] if "batch_size" in options else 1
+
+    def _tokens_log_prob_single_batch(
+        self, text: List[str]
+    ) -> List[Tuple[torch.FloatTensor, torch.LongTensor, List[str]]]:
+
         device = self.model.device
-        input_text = "%s%s%s" % (
-            self.tokenizer.bos_token,
-            text,
-            self.tokenizer.eos_token,
-        )
-        # len(tokens) = seq_len + 2
-        tokens = self.tokenizer.tokenize(input_text)
-        # ids.shape = [1, seq_len + 2]
-        ids = torch.tensor(  # pylint: disable=not-callable
-            [self.tokenizer.convert_tokens_to_ids(tokens)],
-            device=device,
-            dtype=torch.long,
+
+        input_text = [
+            self.tokenizer.bos_token + sentence + self.tokenizer.eos_token
+            for sentence in text
+        ]
+
+        tokens_list = [self.tokenizer.tokenize(sentence) for sentence in input_text]
+
+        tokens_ids_list = [
+            torch.tensor(  # pylint: disable=not-callable
+                self.tokenizer.convert_tokens_to_ids(tokens),
+                device=device,
+                dtype=torch.long,
+            )
+            for tokens in tokens_list
+        ]
+
+        ids = pad_sequence(
+            tokens_ids_list, batch_first=True, padding_value=self.tokenizer.eos_token_id
         )
 
         with torch.no_grad():
             outputs = self.model(ids)
 
-        # pred_scores.shape = [1, seq_len + 2, vocab_size]
+        # pred_scores.shape = [nb_sentences, max_seq_len, vocab_size]
         pred_scores = outputs[0]
 
-        # len(tokens) = seq_len + 1
-        tokens = tokens[1:]
-        # ids.shape = [1, seq_len + 1, vocab_size]
+        # Align input and target
         ids = ids[:, 1:]
-        # pred_scores.shape = [1, seq_len + 1, vocab_size]
         pred_scores = pred_scores[:, :-1, :]
 
-        # ids_scores.shape = [1, seq_len + 1]
+        # Retrieve the token scores corresponding to the target id
+        # ids_scores.shape = [nb_sentences, max_seq_len]
         ids_scores = pred_scores.gather(2, ids.unsqueeze(2)).squeeze(2)
-        # log_prob.shape = [1, seq_len + 1]
+
+        # log_prob.shape = [nb_sentences, max_seq_len]
         log_probs = ids_scores - pred_scores.logsumexp(2)
 
-        return log_probs[0], ids[0], tokens  # type: ignore
+        outputs = []
+        for i in range(len(text)):
+            output = (
+                log_probs[i, : len(tokens_list[i]) - 1],
+                ids[i, : len(tokens_list[i]) - 1],
+                tokens_list[i][1:],
+            )
+            outputs.append(output)
+
+        return outputs  # type: ignore
 
     def _tokens_log_prob(
-        self, text: List[str]
+        self, sentences: List[str]
     ) -> List[Tuple[torch.FloatTensor, torch.LongTensor, List[str]]]:
-        return list(map(self._tokens_log_prob_single_sentence, text))
 
-    # @overrides
+        output = []
+        for i in range(0, len(sentences), self.batch_size):
+            batch = sentences[i : i + self.batch_size]
+            output += self._tokens_log_prob_single_batch(batch)
+
+        if len(sentences) % self.batch_size != 0:
+            remaining_batch = sentences[-(len(sentences) % self.batch_size) :]
+            output += self._tokens_log_prob_single_batch(remaining_batch)
+
+        return output
+
+    # @overrides_tokens_log_prob_single_batch
     @classmethod
     def _supported_model_names(cls) -> Iterable[str]:
         return GPT2LMHeadModel.pretrained_model_archive_map.keys()
