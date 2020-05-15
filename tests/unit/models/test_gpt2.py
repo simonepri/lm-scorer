37eb5586dca166ffee2c1eff1e659cfb9d1cec72
dldk-gael <gael@de-leseleuc.com>
2020-05-15 14:42:42 +0200

    add a temporary test for overflowing tokens

--- tests/unit/models/test_gpt2.py
+++ tests/unit/models/test_gpt2.py
@@ -106,3 +106,11 @@ def describe_tokens_log_prob_for_batch():
                 assert math.isclose(score, exp_score, rel_tol=eps), {"index": (i, j)}
             assert ids == exp_ids
             assert tokens == exp_tokens
+
+    @pytest.mark.xfail
+    def should_work_with_a_sentence_longer_than_the_model_max_size():
+        max_input_size = scorer.tokenizer.max_len
+        long_sentence = ["The " * (max_input_size + 1)]
+        warning = "One of the sentences is longer than the model max size."
+        with pytest.warns(UserWarning, warning):
+            scorer._tokens_log_prob_for_batch(long_sentence)
